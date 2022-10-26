328890281790084a71b621c850be792e41a1a298
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-26 19:16:28 +0200

    Change meta

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,17 @@
 import styles from './globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Subscription Manager</title>
+        <meta name="description" content="Subscription Manager" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
