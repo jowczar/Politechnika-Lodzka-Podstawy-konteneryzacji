e702bd01b69cd7cb8c1daa4f32d13b9ba3f0124c
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-29 17:18:35 +0200

    Refactor AccountMenu to separated component

---
+++ frontend/components/Navbar/AccountMenu/AccountMenu.stories.js
@@ -0,0 +1,11 @@
+import AccountMenu from "./AccountMenu";
+
+export default {
+  title: "Components/Navbar/AccountMenu",
+  component: AccountMenu,
+};
+
+const Template = (args) => <div className="flex items-center justify-center vh-full vw-full"><AccountMenu {...args} /></div>;
+
+export const Menu = Template.bind({});
+Menu.args = { user: { avatar: "https://i.pravatar.cc/300", email: "example@youtube.com" } };
