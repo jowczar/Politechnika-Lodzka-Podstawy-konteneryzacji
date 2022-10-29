fc985cc17e1a356d0b4a93aa3001ecfd02652c5c
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-29 20:43:45 +0200

    Add props to single channel page

--- frontend/pages/channel/[name].js
+++ frontend/pages/channel/[name].js
@@ -1,3 +1,32 @@
-export default function Channel() {
-  return <h1>Single channel page</h1>
+const Channel = ({ isFound, videos, name }) => {
+    if (!isFound) {
+        return <h1>Channel not found</h1>
+    }
+
+    return <h1>Single channel page</h1>
 }
+
+export function getServerSideProps({ params }) {
+    const { name } = params;
+
+    //  TODO: ask real API if channel exists & get its data
+    if (name === 'i-dont-exist') {
+        return {
+            props: {
+                isFound: false,
+                videos: [],
+                name
+            }
+        }
+    }
+
+    return {
+        props: {
+            isFound: true,
+            videos: [{ id: 1, title: 'Video 1' }, { id: 2, title: 'Video 2' }],
+            name
+        }
+    }
+}
+
+export default Channel;
