e45c61a51f60e1e0046a6d877981e2532c45af7c
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-30 21:33:53 +0100

    Add basic channels page content

--- frontend/pages/channels.js
+++ frontend/pages/channels.js
@@ -1,7 +1,90 @@
+import { useState } from "react";
+import { subMinutes } from "date-fns";
+import VideoCard from "../components/VideoCard/VideoCard";
+
 const Channels = () => {
+  // TODO: fetch channels from backend
+  const [channels, setChannels] = useState([
+    {
+      id: 1,
+      name: 'Uwaga! Naukowy bełkot',
+      avatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+      videos: [
+        {
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Mateusz Socha',
+      avatar: 'https://yt3.ggpht.com/ytc/AMLnZu-ORundEEZh9tQovfx4jRoxmYdR7AxHQ3iK5FQtfw=s88-c-k-c0x00ffffff-no-rj', 
+      videos: []
+    }
+  ]);
+
   return (
-    <div className="flex flex-col gap-3 items-center justify-center lg:h-screen mt-20 lg:mt-0">
-        Channels
+    <div>
+      {/* TODO: filters */}
+      {channels.map((channel) => 
+        <div key={"channel_" + channel.id}>
+          <div className="flex flex-row gap-2 items-center mx-10 first:mt-10">
+            <img className='w-7	h-7 rounded-full' src={channel.avatar} alt={'avatar'} />
+            <h2 className="font-bold text-2xl	">{channel.name}</h2>
+            {channel.videos.length === 0 && <div className="font-light text-2xl text-gray-400"> • you are all caught up ✨</div>}
+          </div>
+          <div className="flex flex-row gap-10 overflow-auto no-scrollbar py-2.5 px-10">
+            {channel.videos.map((video) => <VideoCard className='shrink-0' video={video} key={"video_" + video.id} />)}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
