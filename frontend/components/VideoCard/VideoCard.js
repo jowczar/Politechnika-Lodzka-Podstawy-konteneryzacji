4848437c35fae880535c19648df43f2b7d9347d3
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-30 09:54:33 +0100

    Add watched variant of video card

--- frontend/components/VideoCard/VideoCard.js
+++ frontend/components/VideoCard/VideoCard.js
@@ -1,13 +1,26 @@
 import { getTimeComment } from './VideoCard.helper'
-import { MdOutlineCheck, MdOutlineDone, MdOutlineRemoveDone } from 'react-icons/md';
-import { IoMdCloseCircle, IoCheckmarkCircleSharp } from 'react-icons/io';
+import { MdOutlineDone, MdOutlineRemoveDone } from 'react-icons/md';
+import { IoMdCloseCircle } from 'react-icons/io';
+import clsx from 'clsx'
 
 export const VideoCard = ({ video: { thumbnail, title, duration, uploadTime, channelName, channelAvatar, isWatched, isHidden }, className }) => {
+
+    const markDone = () => { console.log('TODO: mark as done'); } 
+
+    const hide = () => { console.log('TODO: mark as hidden'); } 
+
+    const markUndone = () => { console.log('TODO: mark as undone'); } 
+
     return (
         <div className={`bg-white w-fit rounded ${className}`}>
-            <div className='relative'>
-                <img src={thumbnail} alt="thumbnail" className="rounded-t h-[200px] w-[355px]" />
+            <div className={'relative transition-all'}>
+                <img src={thumbnail} alt="thumbnail" className={clsx("rounded-t h-[200px] w-[355px]", isWatched && "opacity-50")} />
                 <h5 className='absolute rounded bottom-1 right-1 font-bold text-[8px] text-white bg-black py-px px-1'>{duration}</h5>
+                {isWatched && (
+                    <div className='absolute opacity-100 top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-green-600 rounded-full p-4'>
+                        <MdOutlineDone className='text-white' size={50} />
+                    </div>
+                )}
             </div>
             <div className='flex flex-row px-5 py-4 gap-3'>
                 <img src={channelAvatar} alt="channel avatar" className="h-9 w-9 rounded-full" />
@@ -19,8 +32,14 @@ export const VideoCard = ({ video: { thumbnail, title, duration, uploadTime, cha
             <div className="flex flex-row h-11 px-5 py-4 bg-grey rounded-b justify-between items-center">
                 <h5 className="text-xs text-gray-500">{getTimeComment(uploadTime)}</h5>
                 <div className='flex flex-row gap-2'>
-                    <MdOutlineCheck className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-green-500' size={24} />
-                    <IoMdCloseCircle className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-red-500' size={24} />
+                    {!isWatched && (
+                        <>
+                            <MdOutlineDone onClick={markDone} className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-green-500' size={24} />
+                            <IoMdCloseCircle onClick={hide} className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-red-500' size={24} />
+                        </>
+                    )}
+                    {isWatched && <MdOutlineRemoveDone onClick={markUndone} className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-gray-500' size={24} />}
+
                 </div>
             </div>
         </div>
--- frontend/components/VideoCard/VideoCard.stories.js
+++ frontend/components/VideoCard/VideoCard.stories.js
@@ -21,3 +21,31 @@ VideoNotWatched.args = {
         isHidden: false 
     } 
 };
+
+export const VideoWatched = Template.bind({});
+VideoWatched.args = { 
+    video: { 
+        thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+        title: 'Dlaczego liście tak robią?', 
+        duration: '18:33', 
+        uploadTime: subMinutes(new Date(), 65), 
+        channelName: 'Uwaga! Naukowy bełkot', 
+        channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+        isWatched: true, 
+        isHidden: false 
+    } 
+};
+
+export const VideoHidden = Template.bind({});
+VideoHidden.args = { 
+    video: { 
+        thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+        title: 'Dlaczego liście tak robią?', 
+        duration: '18:33', 
+        uploadTime: subMinutes(new Date(), 65), 
+        channelName: 'Uwaga! Naukowy bełkot', 
+        channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+        isWatched: false, 
+        isHidden: true 
+    } 
+};
