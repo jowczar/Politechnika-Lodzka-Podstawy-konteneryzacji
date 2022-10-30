a89ecf464d98a24b1b933fd61910571639281680
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-30 23:06:11 +0100

    Add groups page content

--- frontend/pages/groups.js
+++ frontend/pages/groups.js
@@ -1,3 +1,131 @@
-export default function Groups() {
-    return <h1>Groups page</h1>
-  }
+import { useState } from "react";
+import { subMinutes } from "date-fns";
+import VideoCard from "../components/VideoCard";
+import FilterCard from '../components/FilterCard';
+import { IoIosCheckmarkCircle } from 'react-icons/io';
+import { BiHide } from 'react-icons/bi';
+import { MdLabel } from 'react-icons/md';
+
+const Groups = () => {
+  // TODO: fetch groups from backend
+  const [groups, setGroups] = useState([
+    {
+      id: 1,
+      name: 'Biologia 🌳',
+      color: '#19B929',
+      videos: [
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        },
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Kosmos 🚀',
+      color: '#4219B9',
+      videos: [
+        {
+          link: 'https://youtu.be/VaZU37y2T_Q',
+          thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+          title: 'Dlaczego liście tak robią?', 
+          duration: '18:33', 
+          uploadTime: subMinutes(new Date(), 65), 
+          channelName: 'Uwaga! Naukowy bełkot', 
+          channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+          isWatched: false, 
+          isHidden: false 
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: 'Muzyka 🤘',
+      color: '#FFBB55',
+      videos: []
+    }
+  ]);
+  const [filters, setFilters] = useState({
+    isWatched: false,
+    isHidden: false,
+  });
+  // TODO: on filters change, filter videos – filter on frontend or send request to backend - it's not yet decided
+
+  return (
+    <>
+      <div className="flex flex-row gap-4 w-full px-10 py-4 justify-end">
+        <FilterCard text='Show watched' icon={<IoIosCheckmarkCircle size={28} />} isActive={filters.isWatched} onClick={(value) => setFilters({ ...filters, isWatched: value})} />
+        <FilterCard text='Show hidden' icon={<BiHide size={24} />} isActive={filters.isHidden} onClick={(value) => setFilters({ ...filters, isHidden: value})} />
+      </div>
+      {groups.map((group) => 
+        <div key={"group_" + group.id} className='mt-4 mb-8'>
+          <div className="flex flex-row gap-2 items-center mx-10">
+            <a href={group.link} target="_blank" rel="noreferrer" className="hover:underline flex flex-row gap-1 items-center">
+              <h2 className="font-bold text-2xl	">
+                {group.name}
+              </h2>
+              <MdLabel style={{ color: group.color }} className='rotate-180' size={22} />
+            </a>
+            {group.videos.length === 0 && <div className="font-light text-xl text-gray-400"> • you are all caught up ✨</div>}
+          </div>
+          <div className="flex flex-row gap-8 overflow-auto no-scrollbar py-2.5 px-10">
+            {group.videos.map((video) => <VideoCard className='shrink-0' video={video} key={"video_" + video.id} />)}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Groups;
