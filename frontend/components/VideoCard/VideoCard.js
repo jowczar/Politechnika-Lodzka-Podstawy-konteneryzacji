f3fea0b5ca20765a2a5d8ad7cc52c42e018f9a03
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-10-30 09:39:11 +0100

    Add icons

--- frontend/components/VideoCard/VideoCard.js
+++ frontend/components/VideoCard/VideoCard.js
@@ -1,4 +1,6 @@
 import { getTimeComment } from './VideoCard.helper'
+import { MdOutlineCheck, MdOutlineDone, MdOutlineRemoveDone } from 'react-icons/md';
+import { IoMdCloseCircle, IoCheckmarkCircleSharp } from 'react-icons/io';
 
 export const VideoCard = ({ video: { thumbnail, title, duration, uploadTime, channelName, channelAvatar, isWatched, isHidden }, className }) => {
     return (
@@ -11,12 +13,15 @@ export const VideoCard = ({ video: { thumbnail, title, duration, uploadTime, cha
                 <img src={channelAvatar} alt="channel avatar" className="h-9 w-9 rounded-full" />
                 <div className='flex flex-col gap-1'>
                     <h3 className="font-bold text-xs">{title}</h3>
-                    <h4 className="text-xs">{channelName}</h4>
+                    <h4 className="text-xs text-gray-600">{channelName}</h4>
                 </div>
             </div>
-            <div className="flex flex-row h-11 px-5 py-4 bg-grey rounded-b">
-                <h5 className="text-xs">{getTimeComment(uploadTime)}</h5>
-                <div>Ch</div>
+            <div className="flex flex-row h-11 px-5 py-4 bg-grey rounded-b justify-between items-center">
+                <h5 className="text-xs text-gray-500">{getTimeComment(uploadTime)}</h5>
+                <div className='flex flex-row gap-2'>
+                    <MdOutlineCheck className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-green-500' size={24} />
+                    <IoMdCloseCircle className='hover:cursor-pointer hover:scale-110 duration-700 hover:text-red-500' size={24} />
+                </div>
             </div>
         </div>
     )
