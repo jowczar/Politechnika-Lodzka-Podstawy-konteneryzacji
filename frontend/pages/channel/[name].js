8cdb583164f43169c7fc9de783b9fc84ed327889
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-11-06 17:14:04 +0100

    List videos and channel name

--- frontend/pages/channel/[name].js
+++ frontend/pages/channel/[name].js
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import Avatar from "../../components/Avatar/Avatar";
+import Button from '../../components/Button';
+import VideoCard from "../../components/VideoCard";
+import { subMinutes } from "date-fns";
+import clsx from 'clsx';
 
-const Channel = ({ isFound, videos, name }) => {
+const Channel = ({ isFound, videos, channelAvatar, name, isSubscribed }) => {
+    const parsedVideos = JSON.parse(videos);
+
+    const toggleSubscription = () => {
+        console.log('TODO: unsubscribe/subscribe to channel 📹');
+    } 
+
     if (!isFound) {
         return (
             <div className='flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2'>
@@ -17,7 +28,23 @@ const Channel = ({ isFound, videos, name }) => {
         )
     }
 
-    return <h1>Single channel page</h1>
+    return (
+        <div className='flex flex-col'>
+            <div className="flex flex-col items-center gap-4 my-24">
+                <Avatar link={channelAvatar} className='w-20 h-20' />
+                <h1 className="font-bold text-4xl">{name}</h1>
+                <Button variant='cta' onClick={toggleSubscription} className={clsx(
+                    'font-bold w-48',
+                    isSubscribed && "bg-gray-400 hover:bg-gray-500"
+                )}>{isSubscribed ? 'Subscribed' : 'Subscribe'}</Button>
+            </div>
+            <div className="flex w-full flex-wrap gap-8 px-4 pb-10 items-center justify-center">
+                {parsedVideos.map(video => (
+                    <VideoCard hideInteractions className='shrink-0' video={video} key={"video_" + video.id} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export function getServerSideProps({ params }) {
@@ -28,8 +55,10 @@ export function getServerSideProps({ params }) {
         return {
             props: {
                 isFound: false,
+                isSubscribed: false,
                 videos: [],
-                name
+                name,
+                channelAvatar: null,
             }
         }
     }
@@ -37,8 +66,275 @@ export function getServerSideProps({ params }) {
     return {
         props: {
             isFound: true,
-            videos: [{ id: 1, title: 'Video 1' }, { id: 2, title: 'Video 2' }],
-            name
+            isSubscribed: false,
+            videos: JSON.stringify([
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 1,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                },
+                {
+                    id: 2,
+                    link: 'https://youtu.be/VaZU37y2T_Q',
+                    thumbnail: 'https://i.ytimg.com/vi/VaZU37y2T_Q/hqdefault.jpg?sqp=-oaymwEjCNACELwBSFryq4qpAxUIARUAAAAAGAElAADIQj0AgKJDeAE=&rs=AOn4CLA7rqrDHuHKdpNda2RHn2k2pCAQUw', 
+                    title: 'Dlaczego liście tak robią?', 
+                    duration: '18:33', 
+                    uploadTime: subMinutes(new Date(), 65), 
+                    channelName: 'Uwaga! Naukowy bełkot', 
+                    channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj', 
+                    isWatched: false, 
+                    isHidden: false 
+                }
+            ]),
+            name,
+            channelAvatar: 'https://yt3.ggpht.com/ArVAdn46mUBoDsd8PV_V4Bpjr8iGdEIbLChyLs2h3949LFhogNJUt9qcSTDDiVk1jHozFaElKtA=s176-c-k-c0x00ffffff-no-rj'
         }
     }
 }
